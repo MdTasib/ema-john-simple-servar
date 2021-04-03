7c3296e31186345af18714a79ff9eb510bcbf61a
OhidulAlam <mdtasib442@gmail.com>
2021-04-03 19:13:47 +0600

    project

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1aqm3.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const productsCollection = client.db("emajohnStore").collection("products");
+    const ordersCollection = client.db("emajohnStore").collection("products");
+
+    // post 
+    app.post('/addProduct', (req, res) => {
+        const products = req.body;
+        productsCollection.insertOne(products)
+            .then(result => {
+                console.log(result.insertedCount);
+                res.send(result.insertedCount);
+            })
+    })
+
+    // get
+    app.get('/products', (req, res) => {
+        productsCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
+
+    app.get('/product/:key', (req, res) => {
+        productsCollection.find({ key: req.params.key })
+            .toArray((err, documents) => {
+                res.send(documents[0]);
+            })
+    })
+
+    // productsByKeys
+    app.post('/productsByKeys', (req, res) => {
+        const productKeys = req.body;
+        productsCollection.find({ key: { $in: productKeys } })
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
+    })
+
+
+    app.post('/addOrder', (req, res) => {
+        const order = req.body;
+        ordersCollection.insertOne(order)
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
+    })
+
+
+});
+
+
+app.listen(4000);
